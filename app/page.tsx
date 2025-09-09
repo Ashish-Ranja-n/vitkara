0af1e7011eddad92dbb9a60da9541edb04cec3c1
Ashish-Ranja-n <7739811628mintu@gmail.com>
2025-09-09 19:58:10 +0530

    added mobile

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
       <Background />
       <div className="relative z-10 flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow flex flex-col items-center justify-center text-center px-4">
-          <h1 className="text-5xl sm:text-7xl font-extrabold mb-4 leading-tight">
+        <main className="flex-grow flex flex-col items-center justify-center text-center px-4 my-16">
+          <h1 className="text-5xl sm:text-6xl font-extrabold mb-4 leading-tight">
             Invest in Main Street.
             <br />
             <span className="text-cyan-400">Empower Local Shops.</span>
           </h1>
-          <p className="text-lg sm:text-xl max-w-3xl mx-auto text-gray-300 mb-8">
+          <p className="text-md font-semibold sm:text-xl max-w-3xl mx-auto text-gray-300 mb-8">
             Vitkara is a micro-finance marketplace that funds small local shops by connecting them with everyday investors.
           </p>
           <button className="bg-cyan-400 text-black font-bold py-3 px-8 rounded-full text-lg hover:bg-cyan-300 transition-transform transform hover:scale-105 flex items-center gap-2">
