b88773f32a86bbca2f1a54b623dee3c2616fb1d6
Ashish-Ranja-n <7739811628mintu@gmail.com>
2025-09-09 19:27:36 +0530

    updates

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useRef, useEffect } from "react";
-import { motion } from "framer-motion";
 
 const Background = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
