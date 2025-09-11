bf5fe1f3c586ffebd5962247701c58d68e5e52b9
Ashish-Ranja-n <7739811628mintu@gmail.com>
2025-09-11 17:00:53 +0530

    updated mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import { FaBolt } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className="w-full flex items-center justify-between p-4 sm:p-6">
+    <header className="w-full flex items-center justify-between p-4 sm:p-6 sticky top-0">
       <div className="flex items-center gap-2">
         <img src="/vitkara_logo3.png" alt="Vitkara Logo" className=" h-[25px] sm:h-[50px] w-auto" />
       </div>
