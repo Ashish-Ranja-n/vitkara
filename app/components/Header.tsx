21501d960eff1d392657095c5a075bb8507e8a36
Ashish-Ranja-n <7739811628mintu@gmail.com>
2025-09-11 16:03:13 +0530

    updated mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="w-full flex items-center justify-between p-4 sm:p-6">
       <div className="flex items-center gap-2">
-        <h1 className="text-2xl font-bold text-white">Vitkara</h1>
+        <img src="/vitkara_logo2.jpg" alt="Vitkara Logo" className=" h-[25px] sm:h-[50px] w-auto" />
       </div>
       
     </header>
