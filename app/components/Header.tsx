d693a7d28a3a07e49a8689347e77e904080fb03d
Ashish-Ranja-n <7739811628mintu@gmail.com>
2025-09-11 17:27:53 +0530

    updated mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
 
   return (
     <header className="w-full flex items-center justify-between p-4 sm:p-6 sticky top-0">
-      <div className={`flex items-center gap-2 transition-all duration-300 ${isScrolled ? 'backdrop-blur-xs bg-cyan-400/20 rounded-md' : ''}`}>
+      <div className={`flex items-center gap-2 transition-all duration-300 ${isScrolled ? 'backdrop-blur-lg bg-cyan-400/10 rounded-md' : ''}`}>
         <img src="/vitkara_logo3.png" alt="Vitkara Logo" className="h-[25px] sm:h-[50px] w-auto" />
       </div>
 
